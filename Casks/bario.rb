16ce9fd3769cbcb6d17a5b2826c837b36b776bed
Tanner <51724788+lightningboltemoji@users.noreply.github.com>
2026-09-17 21:40:11 -0700

    bario placeholders

---
+++ Casks/bario.rb
@@ -0,0 +1,34 @@
+cask "bario" do
+  version "0.0.0"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+
+  url "https://github.com/lightningboltemoji/bario/releases/download/v#{version}/bario-#{version}.zip"
+  name "Bario"
+  desc "Customizable menu bar overlay"
+  homepage "https://github.com/lightningboltemoji/bario"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  conflicts_with cask: "lightningboltemoji/tap/bario@tip"
+  depends_on macos: :tahoe
+  depends_on arch:  :arm64
+
+  app "Bario.app"
+  binary "#{appdir}/Bario.app/Contents/MacOS/bario"
+
+  postflight_steps do
+    run "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "{{appdir}}/Bario.app"]
+  end
+
+  uninstall quit:       "zip.tanner.bario",
+            login_item: "Bario"
+
+  zap trash: [
+    "~/.config/bario",
+    "~/Library/Preferences/zip.tanner.bario.plist",
+    "~/Library/Saved Application State/zip.tanner.bario.savedState",
+  ]
+end
---
+++ Casks/bario@tip.rb
@@ -0,0 +1,35 @@
+cask "bario@tip" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/lightningboltemoji/bario/releases/download/tip/bario-tip.zip"
+  name "Bario"
+  desc "Customizable menu bar overlay (tip)"
+  homepage "https://github.com/lightningboltemoji/bario"
+
+  conflicts_with cask: "lightningboltemoji/tap/bario"
+  depends_on macos: :tahoe
+  depends_on arch:  :arm64
+
+  app "Bario.app"
+  binary "#{appdir}/Bario.app/Contents/MacOS/bario"
+
+  postflight_steps do
+    run "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "{{appdir}}/Bario.app"]
+  end
+
+  uninstall quit:       "zip.tanner.bario",
+            login_item: "Bario"
+
+  zap trash: [
+    "~/.config/bario",
+    "~/Library/Preferences/zip.tanner.bario.plist",
+    "~/Library/Saved Application State/zip.tanner.bario.savedState",
+  ]
+
+  caveats do
+    <<~EOS
+      the tip build may be unstable. versioned release is suggested.
+    EOS
+  end
+end
